8b1497aaa113cc246286cefa828266bb86726206
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-24 17:18:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 020508
+let accountEmail = "shiv@google.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountID = 2 // not allowed
+
+accountEmail = "some@son.com"
+accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+console.log(accountID);
+
+console.table([accountEmail, accountPassword, accountCity,accountState])
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
