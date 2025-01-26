c6bddca361df67780450bb67325b326b66a57afc
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-26 11:55:37 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // use it for coding in newer version of java
+
+// alert(3 + 3) // you are using nodejs not on browser
+
+ console.log(3 + 3); console.log("Shivanshu"); // code readability is important
+
+let name = "Shivanshu"
+let age = 20
+let isLoggedIn = false
+
+// number range is upto 2^53
+//bigint , not very useful
+// string => ""
+// boolean => true/false
+// null => standalone value ,empty, type of an object
+// unidentified => jab variable declare kiya hai but koi value aasign nahi ki hai
+// symbol => to differ one component from other
+
+//object
+
+console.log(typeof "Shivanshu");
+
+console.log(typeof null);
