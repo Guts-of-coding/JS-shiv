3bbe1e3266b43cc429875ce26ff0bb5b19b6665c
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-26 12:27:45 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = false
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score) // it basically converts
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => gets easily convert into 33 as number
+// "33abc" => converts to NaN i.e, Not A Number
+// "true" => gets convert into 1 and false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// true => 1 and false => 0
+// "" => false
+// "any value" => true
+
+let newName = 44
+
+let stringName = String(newName)
+console.log(stringName);
+console.log(typeof stringName);
+
+
+
+
