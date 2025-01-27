d70746ee0ce4c54111bc34f7e6db78cdf4052e33
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-27 17:27:16 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,8 +23,54 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let newName = 44
 
 let stringName = String(newName)
-console.log(stringName);
-console.log(typeof stringName);
+//console.log(stringName);
+//console.log(typeof stringName);
+
+// ***************************** Operations ***************************** 
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2); */
+
+/*str1 = "hello"
+str2 = "GUTS"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1 + "2");
+console.log("1" + 2 +2); // If string si first then rest of the values are also treated as string
+console.log(1 + 2 + "2"); // If the last value is string then first values are treated as numeric */
+
+// use parenthesis for defining operations in JS
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
+
+
+
+
+
 
 
 
