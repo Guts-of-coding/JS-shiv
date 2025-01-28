20e31adeb43f50a1b2d5b37ed92ae8ab4bf42ff1
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-28 14:57:27 +0000

    comparison of datatypes

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -4,9 +4,22 @@
 // console.log( 2 == 1);
 // console.log( 2 != 1);
 
-console.log("2" > 1);
-console.log("02" > 1);
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+// in JS "==" and "===" boht work in a different way
+// "==" assigns the value, isme conversion hota hai
+// "===" checks if the datatype is same while comparison, it strictly checks
+
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
 
 
-
-
+//  this type of comparisons are mostly avoided 
