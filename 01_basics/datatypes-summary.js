f15100417332aa0cd90421c0b2fbef622e5eb865
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-29 18:33:04 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,33 @@
+// Primitive
+
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Javascript is dynamically typed language
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 45795744626876n
+
+// Reference (Non Primtive)\
+
+// Array, Objects, Funtions
+
+
+
+const heroes = ["ben","thor","hulk"];
+let myObj = {
+    name : "shiv",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+// non primitive has return type object
+// function return type is object function
