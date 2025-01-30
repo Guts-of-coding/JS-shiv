1c716b919bf3607b4dba3f4e3e106fa251794fdb
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-30 16:51:30 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,12 @@
+const name = "Shivanshu"
+const lastName = "Singh"
+
+// console.log(name + lastName + "Guts"); known as string concatenation ,not a good way for joining strings 
+
+// ***********
+// Another Method
+
+console.log(`My name is ${name} and my lastName is ${lastName}`);
+
+
+
