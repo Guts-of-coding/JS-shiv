ee35f3490f2c55303950aa941b11b196140ee592
Guts-of-coding <singhshivanshu216@gmail.com>
2025-01-31 17:08:00 +0000

    strings

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -10,3 +10,30 @@ console.log(`My name is ${name} and my lastName is ${lastName}`);
 
 
 
+const gameName = new String(`GUTSSSS`)
+
+console.log(gameName[0]);
+console.log(gameName[4]);
+
+
+// Another way to access key value in Object
+
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase);
+console.log(gameName);
+
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf(`SS`));
+
+
+const newString = gameName.substring(0,4)
+console.log(newString); // isme negative values declare nahi kar sakte
+
+const anotherString = gameName.slice(-3, 1) // isme negative values declare kar sakte hai
+console.log(anotherString);
+
+
+
+
+
