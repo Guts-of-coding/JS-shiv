3274e1266fc5a71ac9cace8b092de4870795308b
Guts-of-coding <singhshivanshu216@gmail.com>
2025-02-01 18:09:32 +0000

    number and maths

---
+++ 01_basics/06_nums-and-maths.js
@@ -0,0 +1,21 @@
+const score = 5
+console.log(score); // iske output mein number hi ata hai but woh define nahi hota hai
+
+const match = new Number(160904) // iske output mein number define karke aata hai
+// console.log(match);
+
+console.log(match.toString()); // to change the datatype to string
+console.log(match);
+
+console.log(match.toFixed(2)); // decimals ke baad kitne numbers tak ans aayega uske liye
+
+
+const anotherNumber = 436.6743
+
+console.log(anotherNumber.toPrecision(3));
+
+const newNumber = 1000000
+
+console.log(newNumber.toLocaleString());
+
+
