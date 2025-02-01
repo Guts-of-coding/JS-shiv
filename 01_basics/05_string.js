b1e3bad3386f8d06eabf872cbd0147eecf6fa6b0
Guts-of-coding <singhshivanshu216@gmail.com>
2025-02-01 15:21:15 +0000

    strings and its objects

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -6,7 +6,7 @@ const lastName = "Singh"
 // ***********
 // Another Method
 
-console.log(`My name is ${name} and my lastName is ${lastName}`);
+// console.log(`My name is ${name} and my lastName is ${lastName}`);
 
 
 
@@ -18,9 +18,9 @@ console.log(gameName[4]);
 
 // Another way to access key value in Object
 
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toLowerCase);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toLowerCase);
 console.log(gameName);
 
 console.log(gameName.charAt(2));
@@ -28,11 +28,22 @@ console.log(gameName.indexOf(`SS`));
 
 
 const newString = gameName.substring(0,4)
-console.log(newString); // isme negative values declare nahi kar sakte
+console.log(newString); // isme negative values declare nahi kar sakte; if you try to give negative values it will ignore it and start from zero
 
-const anotherString = gameName.slice(-3, 1) // isme negative values declare kar sakte hai
+const anotherString = gameName.slice(-5, 6) // isme negative values declare kar sakte hai
 console.log(anotherString);
 
+
+const newStringOne = "   GUTS     "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://GUTS.com/shivanshu%20singh"
+
+console.log(url.replace('%20', ''));
+
+console.log(url.includes('kill'));
 
 
 
