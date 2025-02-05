318d20ba3748d497d896c17f75e5762973294d96
MESSI-of-coding <messiisgoatinfootball@gmail.com>
2025-02-05 21:35:18 +0530

    Maths

--- 01_basics/06_nums-and-maths.js
+++ 01_basics/06_nums-and-maths.js
@@ -1,13 +1,13 @@
-const score = 5
-console.log(score); // iske output mein number hi ata hai but woh define nahi hota hai
+// const score = 5
+// console.log(score); // iske output mein number hi ata hai but woh define nahi hota hai
 
-const match = new Number(160904) // iske output mein number define karke aata hai
+// const match = new Number(160904) // iske output mein number define karke aata hai
 // console.log(match);
 
-console.log(match.toString()); // to change the datatype to string
-console.log(match);
+// console.log(match.toString()); // to change the datatype to string
+// console.log(match);
 
-console.log(match.toFixed(2)); // decimals ke baad kitne numbers tak ans aayega uske liye
+// console.log(match.toFixed(2)); // decimals ke baad kitne numbers tak ans aayega uske liye
 
 
 const anotherNumber = 436.6743
@@ -16,6 +16,31 @@ console.log(anotherNumber.toPrecision(3));
 
 const newNumber = 1000000
 
-console.log(newNumber.toLocaleString());
+console.log(newNumber.toLocaleString('en-IN'));
+
+// ****************** Math ******************
+
+// console.log(Math);
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.1))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(4, 3 , 5, 7));
+// console.log(Math.max(4, 3, 9, 7));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); // random number between 10 and 20
+
+
+
+
+
+
 
 
