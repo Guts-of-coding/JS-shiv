d5eb950c6db45d0e58443d0d34407e4545d0eccf
MESSI-of-coding <messiisgoatinfootball@gmail.com>
2025-05-22 08:24:18 +0530

    My Portfolio

--- 02_basics/04_Objects2.js
+++ 02_basics/04_Objects2.js
@@ -19,7 +19,7 @@ const regularUser = {
     }
 }
 
-console.log(regularUser.fullname.anotherregularUser.lastname); // Output: Berserk
+//console.log(regularUser.fullname.anotherregularUser.lastname); // Output: Berserk
 
 
 const kill1 ={1: "Guts", 2: "Thorfinn", 3: "Climber"}
@@ -30,28 +30,70 @@ const kill3 ={7: "Clover", 8: "Yami"}
 
 // const allkills = Object.assign({}, kill1, kill2, kill3)      very less used; the curly braces are empty because all the values of the objects
                                                                  // will be stored in th empty object
+                                                                 // mostly asked
 
 const allkills ={...kill1, ...kill2, ...kill3} // spread operator; mostly used
 
 
 // console.log(allkills);
 
-const user = {
+// AISA TAB USE KARTE HAI JAB DATABSE SE VALUES AATA HAI 
+// VALUES EK ARRAY OF OBJECTS MEIN AATE HAI
+
+const user = [
+    {
     name: "Guts",
     age: 27,
-}
-const user2 = {
+},
+ {
     name: "Guts",
     age: 27,
-}
-const user3 = {
+},
+ {
     name: "Guts",
     age: 27,
-}
-const user4 = {
+},
+ {
     name: "Guts",
     age: 27,
+},
+]
+
+user[2].name
+// console.log(newUser2);
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+
+
+// console.log(newUser2.hasOwnProperty("name")); // checks if the object has the property or not
+
+
+//OBJECT DESTRUCTURING
+
+const course = {
+    courseName: "Javascript",
+    price: 999,
+    courseInstructor: "GUTS",
 }
+
+const {courseInstructor} = course
+// console.log(courseInstructor);
+
+//console.log(course.courseName);
+
+// API KYA HOTI HAI?
+// APNA KAAM DUSRE PAR THOPNA YA FIR DUSRE SE KARWANA 
+// FOR EG: ORDERING FOOD IN HOTEL OR ONLINE
+// BACKEND SE KUCH VALUES AATI HAI , UN VALUES KO AAP KAISE LIKHTE HAI WOH HOTA HAI API
+// VALUES COME IN THE FORM OF JSON
+
+{
+    "name": "GUTS",
+    "age": 27,
+   " email": "Guts@mail.com",
+}
+
 
 
 
---
+++ 03_basics/01_functions.js
@@ -0,0 +1,79 @@
+function whoistheRealGoat(){                //// isse kehte function
+    // console.log("Messi is the Goat");    
+    // console.log("Messi is the Goat");    // isse kehte hai function definition , jo bhi kuch curly braces ke andar likha hai usse
+    // console.log("Messi");
+    // console.log("Messi");
+    // console.log("Messi");
+    // console.log("Messi");
+    
+}
+
+whoistheRealGoat()  // round bracket lagane se function run hota hai
+
+// function declare karte wakt jo round bracket mein pass karte hai usse kehte hai "Parameters". isme datatype batana 
+//  hota hai 
+
+function WorldcupWinner(number1, number2){
+    // console.log(number1 + number2);
+    
+}
+
+WorldcupWinner(1, "0") // Aur jo function call karte wakt value pass ki jati hai usse kehte hai Arguements
+
+function addTwoNumbers(num1, num2){
+    let result = num1 + num2   //result ko ek variable mein pehle store kar liya hai
+
+    //fir hum likhenge return result 
+    return result
+
+    //ab return ke baad kuch bhi likhenge toh woh execute nahi hoga
+    // jaise ki
+    console.log("Shivanshu");
+
+    // ya fir itna likhne se aacha hai ki aap seedha hi return resutl likh do
+    return num1 + num2
+    
+}
+
+const result = addTwoNumbers(3, 7)
+
+// console.log("Result: ",result );
+
+function loginUserMessage(username){
+    return `${username} has logged in`
+}
+
+// console.log(loginUserMessage("Shivanshu"));
+
+
+function calculateCartPrice(num1){
+    return num1
+}
+calculateCartPrice()
+console.log(calculateCartPrice(7));
+
+
+// par cart mein customer items add karte chala jata hai toh fir single item hi show karega 
+// baaki ke items ke liye we cant write num1,num2,..... and so on
+// isiliye we use rest and spread operator
+
+// lets take the code
+
+function calculateCartPrice(...num1){
+    return num1
+}
+calculateCartPrice()
+console.log(calculateCartPrice(7,100,2,300,400,600)); // the 3 dots is the rest operator. isse use karne se kam se kam 
+                                                        // value ek array mein toh store hoti hai
+
+const user = {
+    username: "GUTS",
+    price: 999
+}   
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and the price is ${anyobject.price}`);
+    
+}
+
+handleObject(user)
