b5dc95a208eb7072b9702c11c9806a94506e88dc
MESSI-of-coding <messiisgoatinfootball@gmail.com>
2025-10-15 16:33:06 +0530

    While loop

---
+++ 05_iterations/02_twoWhile.js
@@ -0,0 +1,33 @@
+// while 
+
+/*while (condition) {
+    
+}*/
+
+let i = 0
+while (i <= 10) {
+    //console.log(`${i}`);
+    i = i + 2
+}
+
+// for array
+
+let myCharNames = ["Guts", "Thors", "Nikashi"]
+
+let newChars = 0
+while (newChars <= myCharNames.length) {
+    //console.log(myCharNames);
+    myCharNames = myCharNames++
+}
+
+
+// do while
+// kaam pehle hota hai condition baad mein check hoti hai
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}` );
+    score++
+} while (score <= 11);
+
